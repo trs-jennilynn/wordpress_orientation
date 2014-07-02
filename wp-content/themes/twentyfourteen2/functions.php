8ee25d82ae51e47d78b39a0bfd8928cb6c7abe3e
trd-paolo <trd-paolo@gmail.com>
2014-07-02 18:38:29 +0800

    add a check if admin panel is not showing, then adjust header position.

--- wp-content/themes/twentyfourteen2/functions.php
+++ wp-content/themes/twentyfourteen2/functions.php
@@ -10,7 +10,20 @@
 	function load_scripts(){
 		wp_enqueue_style("main-style", get_stylesheet_uri());
 		wp_enqueue_script("main-script", get_template_directory_uri()."/scripts/script.js",array("jquery"),TRUE);
+		
+	
 	}
+	
+	function adjust_header(){
+		if ( !is_admin_bar_showing() ) {
+			echo '<style type="text/css">';
+			echo '#header-wrapper {top:0;}';
+			echo '</style>';
+		}
+	}
+	
+	add_action("wp_head","adjust_header");
+	
 	
 	add_action("wp_enqueue_scripts","load_scripts");
 	
